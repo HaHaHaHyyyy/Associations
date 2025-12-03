e1c79e4dd82963b23babc851daefaf9508164a96
Dasha <you@example.com>
2025-12-03 21:39:40 +0300

    start

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import os
+clear = lambda: os.system('cls')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+words = ['кафе','зарплата','брюнет','автомобиль','звонок','бабушка','цель','жена','гражданин','театр','телевизор','хозяин','интернет','учитель']
 
+points = {'Player 1': 0, 'Player 2': 0, 'Player 3': 0,'Player 4': 0,'Player 5': 0}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+adj = {'Player 1': [], 'Player 2': [], 'Player 3': [],'Player 4': [],'Player 5': []}
 
+verb = {'Player 1': [], 'Player 2': [], 'Player 3': [],'Player 4': [],'Player 5': []}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+noun = {'Player 1': [], 'Player 2': [], 'Player 3': [],'Player 4': [],'Player 5': []}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+number_of_players=int(input('Число игроков: '))
+
+current_player = 0
+for i in range(0,number_of_players):
+
+    print('Player: ', current_player + 1)
+
+    for j in range(0,3):
+        adj[current_player] = input('Введите прилагательные')
+
+    for j in range(0,3):
+        verb[current_player] = input('Введите глаголы')
+
+    for j in range(0,3):
+        noun[current_player] = input('Введите существительные')
+
+#while not 15 in points.values():
+
+clear()
+
+print(1111)
